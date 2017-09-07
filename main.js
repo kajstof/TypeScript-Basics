a180fc15f105d7c38195e5c0849e217a81b508b1
Krzysztof Krysiak <krysiak86@gmail.com>
2017-09-07 15:17:10 +0200

    Add new lessons (024 Properties)

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // TypeScript == JavaScript
-function log(message) {
+function log1(message) {
     console.log(message);
 }
-var message = 'Hello World';
-log(message);
+var message1 = 'Hello World';
+log1(message1);
 // 'let' keyword
 function doSomething() {
     for (var i = 0; i < 5; i++) {
@@ -14,7 +14,7 @@ function doSomething() {
 doSomething();
 // 'let' is statically types
 var count = 5;
-var aa;
+var aa; // let aa;
 aa = 1;
 aa = true;
 aa = 'a';
@@ -39,3 +39,75 @@ var Color;
 })(Color || (Color = {}));
 ;
 var backgroundColor = Color.Blue;
+// Type assertion
+var msg1 = 'abc';
+var endsWithC = msg1.endsWith('c');
+var msg2;
+msg2 = 'abc';
+var endsWithC21 = msg2.endsWith('c');
+var endsWithC22 = msg2.endsWith('c');
+// Arrow functions
+var log2 = function (message) {
+    console.log(message);
+};
+var doLog = function (message) { return console.log(message); };
+var doSth = function () { return console.log(); };
+// Interfaces
+var drawPoint1 = function (point) { };
+drawPoint1({ x: 1, y: 2 });
+var drawPoint2 = function (point) { };
+var getDistance = function (pointA, pointB) { };
+// Claseses
+var Point = /** @class */ (function () {
+    // private x: number;
+    // private y: number;
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+        // this.x = x;
+        // this.y = y;
+    }
+    Point.prototype.draw = function () {
+        console.log('X: ' + this.x + ', Y: ' + this.y);
+    };
+    Object.defineProperty(Point.prototype, "X", {
+        // getX() { return this.x; }
+        // setX(value) {
+        //     if (value < 0) {
+        //         throw new Error('value cannot be less than 0.')
+        //     }
+        //     this.x = value;
+        // }
+        get: function () { return this.x; },
+        set: function (value) {
+            if (value < 0) {
+                throw new Error('value cannot be less than 0.');
+            }
+            this.x = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "Y", {
+        get: function () { return this.y; },
+        set: function (value) {
+            if (value < 0) {
+                throw new Error('value cannot be less than 0.');
+            }
+            this.x = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Point.prototype.getDistance = function (another) { };
+    return Point;
+}());
+var point1 = new Point();
+var point2 = new Point(1, 2);
+// point2.getX();
+// point2.setX(10);
+var xx = point2.X;
+point2.Y = 10;
+var yy = point2.Y;
+point2.Y = 10;
+point2.draw();
--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 // TypeScript == JavaScript
-function log(message){
+function log1(message) {
     console.log(message);
 }
-var message = 'Hello World';
-log(message);
+var message1 = 'Hello World';
+log1(message1);
 
 // 'let' keyword
 function doSomething() {
-    for (let i = 0; i< 5; i++){
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
     console.log('Finally: ' + i);
@@ -16,7 +16,7 @@ doSomething();
 
 // 'let' is statically types
 let count = 5;
-let aa: number
+let aa : any; // let aa;
 aa = 1;
 aa = true;
 aa = 'a';
@@ -37,3 +37,73 @@ const ColorBlue = 2;
 // 'enum' keyword in TranScript
 enum Color { Red = 0, Green = 1, Blue = 2, Purple = 3 };
 let backgroundColor = Color.Blue;
+
+// Type assertion
+let msg1 = 'abc';
+let endsWithC = msg1.endsWith('c');
+let msg2;
+msg2 = 'abc';
+let endsWithC21 = (<string>msg2).endsWith('c');
+let endsWithC22 = (msg2 as string).endsWith('c');
+
+// Arrow functions
+let log2 = function (message) {
+    console.log(message);
+}
+let doLog = (message) => console.log(message);
+let doSth = () => console.log();
+
+// Interfaces
+let drawPoint1 = (point: { x: number, y: number }) => { /* .... */ }
+drawPoint1({ x: 1, y: 2 })
+interface IPoint {
+    x: number,
+    y: number,
+    draw: () => void
+}
+let drawPoint2 = (point: IPoint) => { /* ... */ }
+let getDistance = (pointA: IPoint, pointB: IPoint) => { /* ... */ }
+
+// Claseses
+class Point {
+    // private x: number;
+    // private y: number;
+    constructor(private x?: number, private y?: number) {
+        // this.x = x;
+        // this.y = y;
+    }
+    draw() {
+        console.log('X: ' + this.x + ', Y: ' + this.y);
+    }
+    // getX() { return this.x; }
+    // setX(value) {
+    //     if (value < 0) {
+    //         throw new Error('value cannot be less than 0.')
+    //     }
+    //     this.x = value;
+    // }
+    get X() { return this.x; }
+    set X(value) {
+        if (value < 0) {
+            throw new Error('value cannot be less than 0.')
+        }
+        this.x = value;
+    }
+    get Y() { return this.y; }
+    set Y(value) {
+        if (value < 0) {
+            throw new Error('value cannot be less than 0.')
+        }
+        this.x = value;
+    }
+    getDistance(another: Point) { /* ... */ }
+}
+let point1 = new Point();
+let point2 = new Point(1, 2);
+// point2.getX();
+// point2.setX(10);
+let xx = point2.X;
+point2.Y = 10;
+let yy = point2.Y;
+point2.Y = 10;
+point2.draw();
